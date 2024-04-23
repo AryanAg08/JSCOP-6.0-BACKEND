6522afaf5537b732f9aec4d5d9f359a493fc3f4f
Kartik Aggarwal <kartikaggarwal2004@gmail.com>
2024-04-23 12:32:17 +0530

    cors changed

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(
             "http://localhost:4000",
             "https://temp-jscop-backend-74c1d15b652d.herokuapp.com",
             "https://jscop.jiitopticachapter.com",
-            "http://143.244.136.203"
+            "http://143.244.136.203",
+            "https://main--jscop6frontend.netlify.app/"
         ],
         credentials: true,
     })
