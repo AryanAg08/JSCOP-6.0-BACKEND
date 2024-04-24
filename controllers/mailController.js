aae3811d8fce9af761660bd074de1136bbf69983
AryanAg08 <aryangoyal90@outlook.com>
2024-04-24 16:57:58 +0530

    Adding Nodemailer

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
 
 const transporter = nodemailer.createTransport({
     service: 'gmail', 
