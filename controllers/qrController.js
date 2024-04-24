e4ade5b950a8f01e307b8cdf43c007afb5c42de8
AryanAg08 <aryangoyal90@outlook.com>
2024-04-25 04:29:42 +0530

    Adding Path

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -66,16 +66,16 @@ const sendTicket = async (email, qr_id) => {
         },
     });
     console.log("Transporter created");
-    
+
     transporter.use(
         "compile",
         hbs({
           viewEngine: {
             extName: ".handlebars",
-            partialsDir: path.resolve("../controllers"),
+            partialsDir: path.resolve("controllers"),
             defaultLayout: 'email',
           },
-          viewPath: path.resolve("../controllers"),
+          viewPath: path.resolve("controllers"),
           extName: ".handlebars",
         })
       );
